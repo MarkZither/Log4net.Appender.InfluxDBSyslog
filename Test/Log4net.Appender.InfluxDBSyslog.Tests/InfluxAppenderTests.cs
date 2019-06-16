2dcd5851ef7bc800d8b82bb91c020930009703a9
Mark Burton <mark.burton@zither-it.co.uk>
2019-06-16 21:09:27 +0200

    add unittest boilerplate

---
+++ Test/Log4net.Appender.InfluxDBSyslog.Tests/InfluxAppenderTests.cs
@@ -0,0 +1,44 @@
+using Log4net.Appender.InfluxDBSyslog;
+using Moq;
+using System;
+using Xunit;
+
+namespace Log4net.Appender.InfluxDBSyslog.Tests
+{
+    public class InfluxAppenderTests : IDisposable
+    {
+        private MockRepository mockRepository;
+
+
+
+        public InfluxAppenderTests()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+
+
+        }
+
+        public void Dispose()
+        {
+            this.mockRepository.VerifyAll();
+        }
+
+        private InfluxAppender CreateInfluxAppender()
+        {
+            return new InfluxAppender();
+        }
+
+        [Fact]
+        public void ActivateOptions_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var unitUnderTest = this.CreateInfluxAppender();
+
+            // Act
+            unitUnderTest.ActivateOptions();
+
+            // Assert
+            Assert.True(true);
+        }
+    }
+}
